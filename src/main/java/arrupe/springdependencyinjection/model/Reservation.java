1cfa22b099ae47c926f6a5778e821a433079a0ca
Boris Estrada <bestrada.ark@gmail.com>
2024-09-28 11:19:59 -0600

    Configurationf for new dto

--- src/main/java/arrupe/springdependencyinjection/model/Reservation.java
+++ src/main/java/arrupe/springdependencyinjection/model/Reservation.java
@@ -4,17 +4,21 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.time.LocalDate;
 
 @Entity
 @Data
+@AllArgsConstructor
 public class Reservation {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private int id;
     private String guestName;
     private LocalDate checkInDate;
     private LocalDate checkOutDate;
+
+    public Reservation() {}
 }
